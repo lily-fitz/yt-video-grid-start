83f9202a1497411568c549ab20e8fc2af9ef7e30
Lily <lily@444s-Air.attlocal.net>
2023-07-20 12:31:40 -0400

    first commit

---
+++ database.js
@@ -0,0 +1,69 @@
+import thumbnail1 from './images/thumbnail1.png'
+import channelavatar1 from './images/channelavatar1.png'
+import thumbnail2 from './images/thumbnail2.png'
+import channelavatar2 from './images/channelavatar2.png'
+import thumbnail3 from './images/thumbnail3.png'
+import channelavatar3 from './images/channelavatar3.png'
+import thumbnail4 from './images/thumbnail4.png'
+import channelavatar4 from './images/channelavatar4.png'
+import thumbnail5 from './images/thumbnail5.png'
+import channelavatar5 from './images/channelavatar5.png'
+import thumbnail6 from './images/thumbnail6.png'
+import channelavatar6 from './images/channelavatar6.png'
+
+export const videos = [
+  {
+    id: 1,
+    thumbnail: thumbnail1,
+    channelAvatar: channelavatar1,
+    channelName: 'The Dodo',
+    title: "Golden Retrievers Can't Stop Hugging Each Other Every Time | The Dodo",
+    views: '437K',
+    timestamp: '5 days ago',
+  },
+  {
+    id: 2,
+    thumbnail: thumbnail2,
+    channelAvatar: channelavatar2,
+    channelName: 'FoodStatic',
+    title: 'TIKTOK FOOD EASY RECIPE ✨ pt.2',
+    views: '1M',
+    timestamp: '3 weeks ago',
+  },
+  {
+    id: 3,
+    thumbnail: thumbnail3,
+    channelAvatar: channelavatar3,
+    channelName: 'Architectural Digest',
+    title: 'Tour this Mid-Century Modern Home',
+    views: '357K',
+    timestamp: '6 months ago',
+  },
+  {
+    id: 4,
+    thumbnail: thumbnail4,
+    channelAvatar: channelavatar4,
+    channelName: 'Memelord',
+    title: 'Marvel Cast being the funniest cast alive for 14 minutes straight',
+    views: '606K',
+    timestamp: '11 months ago',
+  },
+  {
+    id: 5,
+    thumbnail: thumbnail5,
+    channelAvatar: channelavatar5,
+    channelName: 'GQ',
+    title: "KEN Things Ryan Gosling Can't Live Without | GQ",
+    views: '1.2M',
+    timestamp: '2 days ago',
+  },
+  {
+    id: 6,
+    thumbnail: thumbnail6,
+    channelAvatar: channelavatar6,
+    channelName: 'ABC News',
+    title: 'Latest News Headlines and Events Worldwide',
+    views: '124K',
+    timestamp: '1 month ago',
+  },
+]
